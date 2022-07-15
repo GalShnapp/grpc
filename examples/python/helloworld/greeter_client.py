f0eb8234223bb10b0272605b03db3fe0ccab325a
Gal Shnapp <galshnapp@Gals-Mac-mini.local>
2022-07-15 12:02:30 +0300

    eyyy

--- examples/python/helloworld/greeter_client.py
+++ examples/python/helloworld/greeter_client.py
@@ -20,18 +20,34 @@ import logging
 import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
+import sys
 
 
-def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
+
+def run(filename):
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
-    print("Greeter client received: " + response.message)
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name=filename))
+        print("client received: " + response.message)
+
+
+
+def secure_run(filename):
+    with open('key.pem.pub', 'rb') as f:
+        creds = grpc.ssl_channel_credentials(f.read())
+    channel = grpc.secure_channel('127.0.0.1:443', creds)
+    stub = helloworld_pb2.GreeterStub(channel)
+    response = stub.SayHello(helloworld_pb2.HelloRequest(name=filename))
+    print("client received: " + response.message)
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
-    run()
+    logging.basicConfig(level=logging.DEBUG)
+    if len(sys.argv) < 2:
+        print(f'don\'t be a smarty (get the reference?), just give us a filename would you?')
+        sys.exit(-1)
+    filename = sys.argv[1]
+    print(filename)
+    #secure_run(str(filename))
+    run(filename)
+pass
--- examples/python/helloworld/greeter_server.py
+++ examples/python/helloworld/greeter_server.py
@@ -19,12 +19,81 @@ import logging
 import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
+import hashlib
+import json
+import os.path
+from os import path
+
+
+PTMD_FILE_PATH = 'persistence/dict.json'
+PATH_TO_MD5_DICT = {}
+
+def persist_ptmd():
+    _json = json.dumps(PATH_TO_MD5_DICT)
+    f = open(PTMD_FILE_PATH,"w")
+
+    # write json object to file
+    f.write(_json)
+
+    # close file
+    f.close()
+
+def load_ptmd():
+    with open(PTMD_FILE_PATH, "r") as file:
+        PATH_TO_MD5_DICT = json.load(file)
+    
+
+def write_kv_to_ptmd(fname: str, md5: str): 
+    PATH_TO_MD5_DICT[fname] = md5
+
+def write_to_ptmv_and_persist(fname: str, md5: str):
+    write_kv_to_ptmd(fname, md5)
+    try:
+        persist_ptmd()
+    except Exception as e:
+        print(e)
+
+def get_val_from_ptmd(filename: str) -> str:
+    return PATH_TO_MD5_DICT.get(filename, None)
+
+def calc_md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return f'{hash_md5.hexdigest()}'
 
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
     def SayHello(self, request, context):
-        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+        fname = request.name
+        if not path.exists(fname):
+            return helloworld_pb2.HelloReply(message=f'error: file {fname} does not exist')
+        try:
+            md5 = calc_md5(fname)
+        except TypeError as te: 
+            return helloworld_pb2.HelloReply(message=f'type error {te}')
+
+        known_md5 = get_val_from_ptmd(fname)
+        write_to_ptmv_and_persist(fname, md5)
+        return helloworld_pb2.HelloReply(message=f'f:{fname}, m_0: {known_md5} m_1:{md5} res {known_md5 != md5}')   
+           
+
+
+
+def secure_serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    with open('key.pem', 'rb') as f:
+        private_key = f.read()
+    with open('key.pem.pub', 'rb') as f:
+        certificate_chain = f.read()
+    server_credentials = grpc.ssl_server_credentials( ( (private_key, certificate_chain), ) )
+    
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    
+    server.add_secure_port('127.0.0.1:443', server_credentials)
+    server.start()
 
 
 def serve():
@@ -36,5 +105,6 @@ def serve():
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    logging.basicConfig(level=logging.DEBUG)
+    #secure_serve()
     serve()
